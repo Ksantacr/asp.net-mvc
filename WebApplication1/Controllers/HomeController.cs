da640ad4b5bf0f3a766e384bab6d0ce7b89a8e2e
Kevin <defonix0@gmail.com>
2018-12-11 22:02:14 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            var model = new InfoModel();
+            model.Name = "Kevin Santacruz";
+            model.Location = "Gye, Ecuador";
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
